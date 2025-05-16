9408979f336ee08c1bad604eca4264e11e8531e8
typedcode <bug@typedcode.de>
2025-05-16 22:51:10 +0200

    renaming env-variables for clarity

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const { Readable } = require("stream");
 
 const app = express();
-const API_URL = `${process.env.MUSIKCUBE_ADDRESS}:${process.env.MUSIKCUBE_PORT}/audio/external_id/`;
+const API_URL = `${process.env.MUSIKCUBE_SERVER_ADDRESS}:${process.env.MUSIKCUBE_STREAMING_PORT}/audio/external_id/`;
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Origin", "*");
     next();
 });
 
-app.get('/api', (req, res) => {
+app.get("/api", (req, res) => {
     func(req, res);
-
 });
 
 async function func(req, res) {
-    const authString = btoa('default:' + process.env.MUSIKCUBE_PASSWORD);
+    const authString = btoa("default:" + process.env.MUSIKCUBE_PASSWORD);
 
     let fetchResponse = await fetch(API_URL + req.query.externalId, {
         headers: {
-            Authorization: 'Basic ' + authString,
-            'Access-Control-Allow-Origin': '*'
-        }
+            Authorization: "Basic " + authString,
+            "Access-Control-Allow-Origin": "*",
+        },
     });
 
     Readable.fromWeb(fetchResponse.body).pipe(res);
 }
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+const PORT = process.env.PROXY_PORT || 5000;
+app.listen(PORT, () =>
+    console.log(`Proxy running and listening on port ${PORT}`),
+);
