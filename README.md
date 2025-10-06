39056b1f75b6d4b2e82a52494b2422ae3a224f63
typedcode <bug@typedcode.de>
2025-10-06 22:29:06 +0200

    Add settings output and installation instructions

--- README.md
+++ README.md
@@ -8,17 +8,19 @@ You need to have node installed to run this project.
 
 ## Installation
 
-- Download the latest Version from [github](https://github.com/typedcode/musikcube-webplayer-proxy) and unzip it to a folder of your choosing.
-- Rename `.env.template` to `.env` and open it in an editor and set at least `MUSIKCUBE_SERVER_ADDRESS` and `MUSIKCUBE_STREAMING_PORT`
+- Clone the repository or download the latest Version from [github](https://github.com/typedcode/musikcube-webplayer-proxy) and unzip it to a folder of your choosing.
+- Rename `.env.template` to `.env` and open it in an editor and set at least `MUSIKCUBE_SERVER_ADDRESS`. Check Parameters below for additional values to set
+- run `npm ci`
+- run `node index.js`
 
 ## Parameters
 
-|Parameter| Description|
-|---------|------------|
-|MUSIKCUBE_SERVER_ADDRESS| The adress where your `musikcube` is running.|
-|MUSIKCUBE_STREAMING_PORT| The port where `musikcube` is open to stream. You can find (and change) this value in musikcube. Start musikcube, go to `settings` and then to `server setup`|
-|MUSIKCUBE_PASSWORD      | If you have set a password to musikcube, you need to set it here|
-|PROXY_PORT              | The port on which the proxy is listening for requests. The default Port is 5000|
+|Parameter               |Default| Description|
+|------------------------|-------|------------|
+|MUSIKCUBE_SERVER_ADDRESS|       | The adress where your `musikcube` is running.|
+|MUSIKCUBE_STREAMING_PORT|7906   | The port where `musikcube` is open to stream. You can find (and change) this value in musikcube. Start musikcube, go to `settings` and then to `server setup`|
+|MUSIKCUBE_PASSWORD      |       | If you have set a password to musikcube, you need to set it here|
+|PROXY_PORT              |5000   | The port on which the proxy is listening for requests. The default Port is 5000|
 
 For more information see [here](https://github.com/typedcode/musikcube-webplayer)
 
--- index.js
+++ index.js
@@ -3,33 +3,45 @@ const express = require("express");
 const { Readable } = require("stream");
 
 const app = express();
-const API_URL = `${process.env.MUSIKCUBE_SERVER_ADDRESS}:${process.env.MUSIKCUBE_STREAMING_PORT}/audio/external_id/`;
+const serverAddress = process.env.MUSIKCUBE_SERVER_ADDRESS;
+const streamingPort = process.env.MUSIKCUBE_STREAMING_PORT || 7906;
+const port = process.env.PROXY_PORT || 5000;
+
+if (serverAddress == undefined || serverAddress == "") {
+  console.error(
+    "Server Adress is not defined. Please check your .env file for the MUSIKCUBE_SERVER_ADDRESS entry",
+  );
+  return;
+}
+
+const API_URL = `${serverAddress}:${streamingPort}/audio/external_id/`;
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
 });
 
 app.get("/api", (req, res) => {
-    func(req, res);
+  func(req, res);
 });
 
 async function func(req, res) {
-    const authString = btoa("default:" + process.env.MUSIKCUBE_PASSWORD);
-    const fetchUrl = API_URL + req.query.externalId;
-    console.log(`Fetching: ${fetchUrl}`);
+  const authString = btoa("default:" + process.env.MUSIKCUBE_PASSWORD);
+  const fetchUrl = API_URL + req.query.externalId;
+  console.log(`Fetching: ${fetchUrl}`);
 
-    let fetchResponse = await fetch(fetchUrl, {
-        headers: {
-            Authorization: "Basic " + authString,
-            "Access-Control-Allow-Origin": "*",
-        },
-    });
+  let fetchResponse = await fetch(fetchUrl, {
+    headers: {
+      Authorization: "Basic " + authString,
+      "Access-Control-Allow-Origin": "*",
+    },
+  });
 
-    Readable.fromWeb(fetchResponse.body).pipe(res);
+  Readable.fromWeb(fetchResponse.body).pipe(res);
 }
 
-const PORT = process.env.PROXY_PORT || 5000;
-app.listen(PORT, () =>
-    console.log(`Proxy running and listening on port ${PORT}`),
-);
+console.log(`Musikcube Server Adress: ${serverAddress}`);
+console.log(`Musikcube Streaming Port: ${streamingPort}`);
+console.log(`Proxy Port: ${port}`);
+
+app.listen(port, () => console.log(`Proxy running... `));
